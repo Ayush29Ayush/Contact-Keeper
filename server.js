0c25642e3e5e02be6b99518c41c6fef0115b0319
Ayush <ayushsenapati123@gmail.com>
2021-10-16 14:14:56 +0530

    Added Home Page Route

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 
 const app = express();
 
+//! This is the Home Page route
 // app.get('/', (req,res)=> res.send('Hello World'))
 app.get("/", (req, res) =>
   res.json({ msg: "Welcome to the ContactKeeper API..." })
