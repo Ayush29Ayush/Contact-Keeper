d43cfa9f9dca52d13f13878b56f05ec8eb031390
Ayush <ayushsenapati123@gmail.com>
2021-10-17 00:45:55 +0530

    Used approach of async await

--- config/db.js
+++ config/db.js
@@ -1,33 +1,34 @@
 const mongoose = require("mongoose");
 const config = require("config");
 // const db = config.get("mongoURI");
-require("dotenv").config()
+require("dotenv").config();
 
 //mongoose returns promises
 
 //! Without async await
-const connectDB = () => {
-  mongoose
-    // .connect(db)
-    .connect(process.env.mongoURI)
-    .then(() => console.log("MongoDB connected"))
-    .catch((err) => {
-      console.error(err.message);
-      process.exit(1);
-    });
+// const connectDB = () => {
+//   mongoose
+//     // .connect(db)
+//     .connect(process.env.mongoURI)
+//     .then(() => console.log("MongoDB connected"))
+//     .catch((err) => {
+//       console.error(err.message);
+//       process.exit(1);
+//     });
+// };
+
+// module.exports = connectDB;
+
+//! With async await
+const connectDB = async () => {
+  try {
+    // await mongoose.connect(db);
+    await mongoose.connect(process.env.mongoURI);
+    console.log("MongoDB connected...");
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
 };
 
 module.exports = connectDB;
-
-//! With async await 
-// const connectDB = async () => {
-//     try{
-//         await mongoose.connect(db);
-//         console.log('MongoDB connected...');
-//     } catch (err){
-//         console.error(err.message);
-//         process.exit(1);
-//     }
-// };
- 
-// module.exports = connectDB;
