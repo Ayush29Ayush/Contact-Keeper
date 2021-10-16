5a6c46855b58fffdc76f6524d22979a4cadd5b5c
Ayush <ayushsenapati123@gmail.com>
2021-10-17 00:14:52 +0530

    Added db.js for database connection

--- config/db.js
+++ config/db.js
@@ -1,3 +1,23 @@
 const mongoose = require("mongoose");
 const config = require("config");
-const db = config.get('mongoURI');
+const db = config.get("mongoURI");
+
+//mongoose returns promises
+
+// Write useNewUrlParser: true,useCreateIndex: true,useFindAndModify: false to avoid warnings
+//! Without async await
+const connectDB = () => {
+  mongoose
+    .connect(db, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    })
+    .then(() => console.log("MongoDB Connected"))
+    .catch((err) => {
+      console.error(err.message);
+      process.exit(1);
+    });
+};
+
+module.exports = connectDB;
