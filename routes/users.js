387d39223ba029cc3e7651965062cba05469f55d
Ayush <ayushsenapati123@gmail.com>
2021-10-16 21:06:24 +0530

    Example for router added

--- routes/users.js
+++ routes/users.js
@@ -9,4 +9,10 @@ router.post('/', (req, res) => {
     res.send('Register a user');
 });
 
+//! Example to understand the router path , basically it concatenates(adds on/merges them) the base url with the url here
+// router.post('/ayush', (req, res) => {
+//     res.send('Register a ayush');
+// });
+
+
 module.exports = router
