40d9d7af2a654f0c874acf831a91eb4c4c18ec10
Ayush <ayushsenapati123@gmail.com>
2021-10-16 14:18:53 +0530

    Home route => res.json({ msg })

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const app = express();
 
 //! This is the Home Page route
 // app.get('/', (req,res)=> res.send('Hello World'))
-app.get("/", (req, res) =>
-  res.json({ msg: "Welcome to the ContactKeeper API..." })
-);
+app.get("/", (req, res) => {
+  res.json({ msg: "Welcome to the ContactKeeper API..." });
+});
 
 const PORT = process.env.PORT || 5000;
 
