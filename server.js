59144a560551375fa6049ae153bca48e1587c5c9
Ayush <ayushsenapati123@gmail.com>
2021-10-16 18:44:29 +0530

    Defined routes in server

--- server.js
+++ server.js
@@ -8,6 +8,11 @@ app.get("/", (req, res) => {
   res.json({ msg: "Welcome to the ContactKeeper API..." });
 });
 
+//! Define Routes
+app.use('/api/users', require('./routes/users'))
+app.use('/api/users', require('./routes/auth'))
+app.use('/api/users', require('./routes/contacts'))
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
