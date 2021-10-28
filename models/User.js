dbc883ab784a2e69756aaa21029970bd63a5365b
Ayush <ayushsenapati123@gmail.com>
2021-10-28 13:19:08 +0530

    Added UserSchema

--- models/User.js
+++ models/User.js
@@ -1,8 +1,24 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const UserSchema = mongoose.Schema({
-    name:{
-        type: String,
-        required: true
-    }
-})
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+//! This means we are exporting our UserSchema by the name of 'user'
+module.exports = mongoose.model("user", UserSchema);
