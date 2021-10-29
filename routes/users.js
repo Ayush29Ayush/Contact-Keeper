774e450cac1d5c670eba6556981fb29ba1cb99ab
Ayush <ayushsenapati123@gmail.com>
2021-10-29 21:45:18 +0530

    Register user and hashed password

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require('bcryptjs')
 const { check, validationResult } = require("express-validator/check");
 
 const User = require("../models/User");
@@ -43,7 +44,27 @@ router.post(
       if (user) {
         return res.status(400).json({ msg: "User already exists" });
       }
-    } catch (err) {}
+
+      // else
+      user = new User({
+        name: name,
+        email: email,
+        password: password,
+        //! using ES6 syntax , since input parameter name equals the variable , we can directly write name,email,password
+      });
+
+      //! Encrypting the password
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      res.send("User saved...")
+
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error...")
+    }
   }
 );
 
