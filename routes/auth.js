a94aea83baa97f11c50e0c212b986a8a36b1e274
Ayush <ayushsenapati123@gmail.com>
2021-10-30 14:27:37 +0530

    Error checking in auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,24 +1,40 @@
 const express = require("express");
 const router = express.Router();
 
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
+const { check, validationResult } = require("express-validator/check");
+
+const User = require("../models/User");
+
 // @route    GET api/auth
 // @desc.    Get logged in user
 // @access   Private route
 
-router.get('/', (req, res) => {
-    res.send('Get logged in user');
+router.get("/", (req, res) => {
+  res.send("Get logged in user");
 });
 
-module.exports = router
-
-
+module.exports = router;
 
 // @route    POST api/auth
 // @desc.    Auth user & get token
 // @access   Public route
 
-router.post('/', (req, res) => {
-    res.send('Log in user');
-});
+router.post(
+  "/",
+  [
+    check("email", "Please include a valid email").isEmail(),
+    check("password", "Password is required").exists(),
+  ],
+  (req, res) => {
+    // res.send("Log in user");
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+  }
+);
 
-module.exports = router
+module.exports = router;
