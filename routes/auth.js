3685d0fb263f48af674923c13e296d00c5d27133
Ayush <ayushsenapati123@gmail.com>
2021-10-30 15:08:32 +0530

    Authenticate Route

--- routes/auth.js
+++ routes/auth.js
@@ -7,6 +7,7 @@ const config = require("config");
 const { check, validationResult } = require("express-validator/check");
 
 const User = require("../models/User");
+const { json } = require("express");
 
 // @route    GET api/auth
 // @desc.    Get logged in user
@@ -28,11 +29,55 @@ router.post(
     check("email", "Please include a valid email").isEmail(),
     check("password", "Password is required").exists(),
   ],
-  (req, res) => {
+  async (req, res) => {
     // res.send("Log in user");
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+
+    try {
+        //! Finding user by email
+      let user = await User.findOne({ email });
+
+      if (!user) {
+        return res.status(400).json({
+          msg: "Invalid Credentials / No user found with this email...",
+        });
+      }
+
+      //! If user is found by the email id then, finding user by password
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      if (!isMatch) {
+        return res.status(400).json({
+          msg: "Invalid Credentials / No user found with this password...",
+        });
+      }
+      //else
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        // gets the jwtSecret from the default.json file in config directory.
+        config.get("jwtSecret"),
+        {
+          expiresIn: 360000,
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error...')
     }
   }
 );
