a8a8264ddea54bffb069cf3d37d7ffb5907a8e64
Ayush <ayushsenapati123@gmail.com>
2021-10-31 17:20:11 +0530

    Added and integrated Navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import Navbar from './components/layout/Navbar';
 
 const App = () => {
   return (
     <div className="App">
+      <Navbar />
       My App
     </div>
   );
