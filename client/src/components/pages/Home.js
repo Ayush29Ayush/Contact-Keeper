c1add68e5552e53fa2252bb56219f5c7f21c0157
Ayush <ayushsenapati123@gmail.com>
2021-11-02 00:27:01 +0530

    Added ContactForm to Homepage

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,12 +1,15 @@
 import React from "react";
 import Contacts from "../contacts/Contacts";
+import ContactForm from "../contacts/ContactForm";
 
 const Home = () => {
   return (
     <div className="grid-2">
-      <div>{/* This will be ContactForm */}</div>
       <div>
-          <Contacts />
+        <ContactForm />
+      </div>
+      <div>
+        <Contacts />
       </div>
     </div>
   );
