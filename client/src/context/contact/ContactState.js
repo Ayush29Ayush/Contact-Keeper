820ba89f1af2cbc43f80d42125379126ff898357
Ayush <ayushsenapati123@gmail.com>
2021-11-01 14:17:13 +0530

    Added Global State

---
+++ client/src/context/contact/ContactState.js
@@ -0,0 +1,71 @@
+import React, { useReducer } from "react";
+import uuid from "uuid";
+import ContactContext from "./contactContext";
+import contactReducer from "./contactReducer";
+import {
+  ADD_CONTACT,
+  DELETE_CONTACT,
+  SET_CURRENT,
+  CLEAR_CURRENT,
+  UPDATE_CONTACT,
+  FILTER_CONTACTS,
+  CLEAR_FILTER,
+} from "../types";
+
+const ContactState = (props) => {
+  const initialState = {
+    contacts: [
+      {
+        id: 1,
+        name: "Sample contact 1",
+        email: "sample1@gmail.com",
+        phone: "111-111-1111",
+        type: "personal",
+      },
+      {
+        id: 2,
+        name: "Sample contact 2",
+        email: "sample2@gmail.com",
+        phone: "222-222-2222",
+        type: "personal",
+      },
+      {
+        id: 3,
+        name: "Sample contact 3",
+        email: "sample3@gmail.com",
+        phone: "333-333-3333",
+        type: "professional",
+      },
+    ],
+  };
+  //! State allows us to access anything in our state.
+  //! Dispatch allows us to dispatch objects to the reducer.
+  const [state, dispatch] = useReducer(contactReducer, initialState);
+
+  //! Actions to be added , will become a CRUD app
+  // Add Contact
+
+  // Delete Contact
+
+  // Set Current Contact
+
+  // Clear Current Contact
+
+  // Update Contact
+
+  // Filter Contact
+
+  // Clear Filter
+
+  return (
+    <ContactContext.Provider
+      value={{
+        contacts: state.contacts
+      }}
+    >
+      {props.children}
+    </ContactContext.Provider>
+  );
+};
+
+export default ContactState;
