d59b9d098905838fc27064634f176fc6090eb3e1
Ayush <ayushsenapati123@gmail.com>
2021-11-02 18:20:45 +0530

    Update Contact

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -67,6 +67,9 @@ const ContactState = (props) => {
   };
 
   // Update Contact
+  const updateContact = (contact) => {
+    dispatch({ type: UPDATE_CONTACT, payload: contact });
+  };
 
   // Filter Contact
 
@@ -80,7 +83,8 @@ const ContactState = (props) => {
         addContact,
         deleteContact,
         setCurrent,
-        clearCurrent
+        clearCurrent,
+        updateContact
       }}
     >
       {props.children}
