b29d6c09ef038f91d1ec05f4b3fed19fbe9b4c79
Ayush <ayushsenapati123@gmail.com>
2021-11-04 10:39:13 +0530

    Added USER_LOADED and AUTH_ERROR

--- client/src/context/auth/authReducer.js
+++ client/src/context/auth/authReducer.js
@@ -11,29 +11,37 @@ import {
 
 export default (state, action) => {
   switch (action.type) {
+    case USER_LOADED:
+      return {
+        ...state,
+        isAuthenticated: true,
+        loading: false,
+        user: action.payload,
+      };
     case REGISTER_SUCCESS:
       localStorage.setItem("token", action.payload.token);
       return {
-          ...state,
-          ...action.payload,
-          isAuthenticated: true,
-          loading: false
+        ...state,
+        ...action.payload,
+        isAuthenticated: true,
+        loading: false,
       };
     case REGISTER_FAIL:
-    localStorage.removeItem('token');    
-    return {
+    case AUTH_ERROR:
+      localStorage.removeItem("token");
+      return {
         ...state,
         token: null,
         isAuthenticated: false,
         loading: false,
         user: null,
-        error: action.payload
-    }
+        error: action.payload,
+      };
     case CLEAR_ERRORS:
       return {
         ...state,
-        error: null
-      }
+        error: null,
+      };
     default:
       return state;
   }
