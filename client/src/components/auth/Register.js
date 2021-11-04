fa1bb2e28f7956c9a47d4d299a033b9f3965c277
Ayush <ayushsenapati123@gmail.com>
2021-11-04 13:22:50 +0530

    Modified useEffect

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -3,20 +3,26 @@ import React, { useContext, useState, useEffect } from "react";
 import AlertContext from "../../context/alert/alertContext";
 import AuthContext from "../../context/auth/authContext";
 
-const Register = () => {
+const Register = (props) => {
   const alertContext = useContext(AlertContext);
   const authContext = useContext(AuthContext);
 
   const { setAlert } = alertContext;
 
-  const { register, error, clearErrors } = authContext;
+  const { register, error, clearErrors, isAuthenticated } = authContext;
 
   useEffect(() => {
+    if (isAuthenticated) {
+      // redirects to the home page if it's authenticated
+      props.history.push("/");
+    }
+
     if (error === "User already exists") {
       setAlert(error, "danger");
       clearErrors();
     }
-  }, [error]);
+    
+  }, [error, isAuthenticated, props.history]);
 
   //password2 is just for password confirmation
   const [user, setUser] = useState({
