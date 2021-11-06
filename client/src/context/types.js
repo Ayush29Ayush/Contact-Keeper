45a4dda2a22edea68ed0bd67ed7e6de80ed63784
Ayush <ayushsenapati123@gmail.com>
2021-11-06 12:53:09 +0530

    Added new types

--- client/src/context/types.js
+++ client/src/context/types.js
@@ -4,7 +4,6 @@
 //The one time you make a typo and type dispatch({ 'SOME_ACTON', payload }) could be a tricky error to track down, especially as we are not logging dispatches anywhere in our app.
 //By using a types module we are declaring our types once in one file/module and then importing the exact same type wherever we need it in our app. This ensures there is no risk of a typo in a string. Obviously if you get a typo in your code for importing or using a variable you get a nice big warning right there in your editor telling you.
 
-
 //! For Frontend part
 export const ADD_CONTACT = "ADD_CONTACT";
 export const DELETE_CONTACT = "DELETE_CONTACT";
@@ -13,6 +12,7 @@ export const CLEAR_CURRENT = "CLEAR_CURRENT";
 export const UPDATE_CONTACT = "UPDATE_CONTACT";
 export const FILTER_CONTACTS = "FILTER_CONTACTS";
 export const CLEAR_FILTER = "CLEAR_FILTER";
+export const CONTACT_ERROR = "CONTACT_ERROR";
 export const SET_ALERT = "SET_ALERT";
 export const REMOVE_ALERT = "REMOVE_ALERT";
 
