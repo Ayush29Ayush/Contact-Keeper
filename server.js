6991262b9ba51c853897508ba3b6f02596b4ce43
Ayush <ayushsenapati123@gmail.com>
2021-11-06 23:19:54 +0530

    production server

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const path = require("path");
 
 const app = express();
 
@@ -12,14 +13,25 @@ app.use(express.json({ extended: false }));
 
 //! This is the Home Page route
 // app.get('/', (req,res)=> res.send('Hello World'))
-app.get("/", (req, res) => {
-  res.json({ msg: "Welcome to the ContactKeeper API..." });
-});
+// app.get("/", (req, res) => {
+//   res.json({ msg: "Welcome to the ContactKeeper API..." });
+// });
 
 //! Define Routes
 app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/contacts", require("./routes/contacts"));
+
+//! Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  // If someone hits the home page , it will go to index.html
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
 
 const PORT = process.env.PORT || 5000;
 
