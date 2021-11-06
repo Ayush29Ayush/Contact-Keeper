b0cd4b35e14c8f7895ae562f2601e75ba9eaa144
Ayush <ayushsenapati123@gmail.com>
2021-11-06 14:19:40 +0530

    imported new types

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -3,12 +3,14 @@ import axios from "axios";
 import ContactContext from "./contactContext";
 import contactReducer from "./contactReducer";
 import {
+  GET_CONTACTS,
   ADD_CONTACT,
   DELETE_CONTACT,
   SET_CURRENT,
   CLEAR_CURRENT,
   UPDATE_CONTACT,
   FILTER_CONTACTS,
+  CLEAR_CONTACTS,
   CLEAR_FILTER,
   CONTACT_ERROR,
 } from "../types";
